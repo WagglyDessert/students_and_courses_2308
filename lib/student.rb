9055454aea198fdfb387ab4adfeb2b2e5ba23bf7
Nathan Trautenberg <Natester123@mac.com>
2023-09-06 09:24:41 -0600

    iteration 1 done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student
-  
+  attr_reader :name, :age, :scores
+  def initialize(student_details)
+    @name = student_details[:name]
+    @age = student_details[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    total_score = @scores.sum.to_f
+    score_count = @scores.count.to_f
+    total_score / score_count
+  end
+
 end
