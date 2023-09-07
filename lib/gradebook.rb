37e9a240421f6ea6c0f054224697fd6edb7d64eb
Nathan Trautenberg <Natester123@mac.com>
2023-09-07 14:05:16 -0600

    finish iteration 4

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -32,4 +32,25 @@ class Gradebook
     return students_below_grade
   end
 
+  def all_grades
+    grades_by_course = Hash.new(nil)
+    @courses.each do |course|
+      grades = course.students.map { |student| student.grade }
+      grades_by_course[course] = grades
+    end
+    grades_by_course
+  end
+
+  def students_in_range(min, max)
+    #array of student objects
+    students_in_sweet_spot = []
+    @courses.each do |course|
+      course.students.each do |student|
+        if student.grade.between?(min, max) == true
+          students_in_sweet_spot << student
+        end
+      end
+    end
+    students_in_sweet_spot
+  end
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -56,4 +56,40 @@ RSpec.describe Course do
     #require 'pry'; binding.pry
     expect(gradebook.students_below(50)).to eq([student2])
   end
+
+  it "can find all grades" do
+    gradebook = Gradebook.new("Mr_T")
+    calculus = Course.new("Calculus")
+    paleontology = Course.new("Paleontology")
+    gradebook.add_course(calculus)
+    gradebook.add_course(paleontology)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    calculus.enroll(student1)
+    calculus.enroll(student2)
+    paleontology.enroll(student1)
+    student1.log_score(89)
+    student1.log_score(78)
+    student2.log_score(45)
+    #require 'pry'; binding.pry
+    expect(gradebook.all_grades).to eq({calculus => [student1.grade, student2.grade], paleontology => [student1.grade]})
+  end
+
+  it 'can find students in a range' do
+    gradebook = Gradebook.new("Mr_T")
+    calculus = Course.new("Calculus")
+    paleontology = Course.new("Paleontology")
+    gradebook.add_course(calculus)
+    gradebook.add_course(paleontology)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    calculus.enroll(student1)
+    calculus.enroll(student2)
+    paleontology.enroll(student1)
+    student1.log_score(89)
+    student1.log_score(78)
+    student2.log_score(45)
+    #require 'pry'; binding.pry
+    expect(gradebook.students_in_range(70, 90)).to eq([student1, student1])
+  end
 end
